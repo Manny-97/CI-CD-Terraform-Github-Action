97884e5f68ea8a012aedecdc05621bc56c3b8a66
Muhammed <muhammed@sendme.ng>
2023-10-31 23:54:04 +0100

    feat: add variables file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY" {
+  description = "AWS Access Key"
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "AWS Secret Access Key"
+}
+
+variable "AWS_REGION" {
+  description = "AWS Region"
+}

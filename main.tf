e6456ca9b91ac0b58089aa760afb078d11ec2db7
Muhammed <muhammed@sendme.ng>
2023-10-31 23:44:40 +0100

    feat: add aws provider

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.21.0"
     }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 resource "aws_s3_bucket" "example" {
